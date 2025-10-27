57e33d231150e463409885f380ac158ae394098a
Genesis <genesis.pena@correo.unimet.edu.ve>
2025-10-26 21:07:10 -0400

    modificaciones pertinentes en nodo y lista, en grafo se cambio de array a lista para usar las edd

--- TodoGrafo/Grafo.java
+++ TodoGrafo/Grafo.java
@@ -2,50 +2,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package TodoGrafo;
+package Grafos;
 import EDD.Nodo;
+import EDD.Lista;
 
 /**
  *
  */
 public class Grafo {
-    private InfoUsuario[] users;
+    private Lista<InfoUsuario> users;
     private int cantidad;
     
-    //se utiliza para pasar la cantidad de usuarios
-    public Grafo(int MaxUsers){
-        users = new InfoUsuario[MaxUsers];
-        cantidad = 0;
+    /** se utiliza para pasar la cantidad de usuarios */
+    public Grafo(){
+        this.users = new Lista<>();
+        this.cantidad = 0;
     }
     
-    //busca el usuario del que se quiere saber
+    /** busca el usuario del que se quiere saber */
     public InfoUsuario searchUser(String nombre){
-        for (int i = 0; i < getCantidad(); i++) {
-            if (getUsers()[i].nombre.equals(nombre)) return getUsers()[i];
-        }return null;
+        Nodo<InfoUsuario> aux = this.users.pfirst;
+        while (aux != null){
+            if (aux.getDato().nombre.equals(nombre)){
+            return aux.getDato();
+            }
+        aux = aux.getPnext();
+        }
+    return null;
     }
     
-    //agrega usuarios
+    /** agrega usuarios */
     public void addUsers(String nombre){
         if (searchUser(nombre) == null) {
-            getUsers()[cantidad++] = new InfoUsuario(nombre);
+            InfoUsuario nuevousuario = new InfoUsuario(nombre);
+            this.users.InsertarFinal(nuevousuario);
+            this.cantidad++;
         }
     }
     
-    //aqui se agrega la relacion que tiene un usuario con otro
+    /** aqui se agrega la relacion que tiene un usuario con otro */
     public void addConexion(String persona, String relacionado){
         InfoUsuario user = searchUser(persona);
         if (user != null){
+            InfoUsuario userrelacion = searchUser(relacionado);
+        if (userrelacion != null){
             user.conexion.InsertarFinal(relacionado);
+            }
         }
     }
     
-    //getters
+    /** getters */
 
     /**
      * @return the users
      */
-    public InfoUsuario[] getUsers() {
+    public Lista<InfoUsuario> getUsers() {
         return users;
     }
 
